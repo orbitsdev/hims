735e8855472141e592e53a4a0bd17587a44a2af9
Brian Orbino <86046953+orbitsdev@users.noreply.github.com>
2024-09-17 11:48:33 +0800

    wip

--- app/Http/Controllers/SendingEmailController.php
+++ app/Http/Controllers/SendingEmailController.php
@@ -19,6 +19,7 @@ class SendingEmailController extends Controller
 
             FilamentForm::notification('success', 'Email was sent successfully.');
         } catch (\Exception $e) {
+            dd($e);
             FilamentForm::error('Failed to send email. Please try again later.');
         }
 

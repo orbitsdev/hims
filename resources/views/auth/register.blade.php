6836934b1954c1f053a3d0ea098f59ed4b290f2c
Brian Orbino <86046953+orbitsdev@users.noreply.github.com>
2024-09-23 09:43:48 +0800

    wip

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,8 +4,8 @@
             <!-- Left Side Form -->
             <div class="w-1/2 p-8">
                 <div class="mb-8">
-                    <h2 class="text-3xl font-bold mb-4">Sign Up to InsideBox</h2>
-                    <p class="text-gray-500">Start your journey</p>
+                    <h2 class="text-3xl font-bold mb-4">Sign Up to HIMS</h2>
+
                 </div>
 
                 <!-- Validation Errors -->
@@ -15,54 +15,59 @@
                 <form method="POST" action="{{ route('register') }}">
                     @csrf
 
-                    <!-- Name Input -->
-                    <div class="mb-6">
-                        <label for="name" class="block text-gray-700 mb-2">Full Name</label>
-                        <input id="name" type="text" name="name" :value="old('name')" required autofocus placeholder="Your full name" class="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-kaitoke-green-500">
-                    </div>
-                    <div class="mb-6">
-                        <label for="username" class="block text-gray-700 mb-2">Username</label>
-                        <input id="username" type="text" name="username" :value="old('username')" required autofocus placeholder="Username" class="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-kaitoke-green-500">
+                    <div class="mt-4">
+                        <x-label for="name" value="{{ __('Fullname') }}" />
+                        <x-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required  />
                     </div>
 
-                    <!-- Email Input -->
-                    <div class="mb-6">
-                        <label for="email" class="block text-gray-700 mb-2">E-mail</label>
-                        <input id="email" type="email" name="email" :value="old('email')" required placeholder="example@email.com" class="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-kaitoke-green-500">
-                    </div>
-
-                    <!-- Password Input -->
-                    <div class="mb-6">
-                        <label for="password" class="block text-gray-700 mb-2">Password</label>
-                        <input id="password" type="password" name="password" required placeholder="********" class="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-kaitoke-green-500">
-                    </div>
-
-                    <!-- Confirm Password Input -->
-                    <div class="mb-6">
-                        <label for="password_confirmation" class="block text-gray-700 mb-2">Confirm Password</label>
-                        <input id="password_confirmation" type="password" name="password_confirmation" required placeholder="********" class="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-kaitoke-green-500">
-                    </div>
-
-                    <!-- Register Button -->
-                    <div class="mb-6">
-                        <button type="submit" class="w-full bg-kaitoke-green-600 text-white py-2 rounded-md hover:bg-kaitoke-green-700 transition">
-                            Register
-                        </button>
-                    </div>
-
-                    <!-- Social Sign-In -->
-                    <div class="text-center text-gray-500 mb-6">or</div>
-                    <a class="flex items-center justify-center w-full border border-gray-200 rounded-lg p-2 hover:bg-gray-100 shadow-sm mb-4">
-                        <img src="https://www.gstatic.com/firebasejs/ui/2.0.0/images/auth/google.svg" alt="Google Icon" class="h-5 w-5 mr-2">
-                        <span class="font-medium text-gray-600">Continue with Google</span>
-                    </a>
-
-                    <!-- Already have an account -->
-                    <div class="text-center">
-                        <p class="text-gray-500">Already have an account? <a href="{{ route('login') }}" class="text-kaitoke-green-600 hover:underline">Sign In</a></p>
-                    </div>
-                </form>
+                    <div class="mt-4">
+                <x-label for="username" value="{{ __('Username') }}" />
+                <x-input id="username" class="block mt-1 w-full" type="text" name="username" :value="old('username')" required  />
             </div>
+            <div class="mt-4">
+                <x-label for="email" value="{{ __('Email') }}" />
+                <x-input id="email" class="block mt-1 w-full" type="text" name="email" :value="old('email')" required  />
+            </div>
+
+            <div class="mt-4">
+                <x-label for="password" value="{{ __('Password') }}" />
+                <x-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
+            </div>
+            <div class="mt-4">
+                <x-label for="password_confirmation" value="{{ __('Confirm Passoword') }}" />
+                <x-input id="password_confirmation" class="block mt-1 w-full" type="password" name="password_confirmation" required autocomplete="password_confirmation" />
+            </div>
+
+            <button class="mt-6 w-full bg-kaitoke-green-700 text-white text-center font-medium py-2 rounded-lg hover:bg-kaitoke-green-800 focus:ring-2 focus:ring-kaitoke-green-600 focus:ring-opacity-50 shadow-md">
+                Register
+              </button>
+            <div class="flex items-center justify-center w-full mt-4">
+                <div class="w-1/3 border-t border-gray-300"></div>
+                <span class="mx-2 text-sm text-gray-400">OR</span>
+                <div class="w-1/3 border-t border-gray-300"></div>
+              </div>
+
+
+              <a class="flex items-center justify-center w-full border border-gray-300 rounded-lg p-2 hover:bg-gray-100 shadow-md mt-4">
+                <img src="https://www.gstatic.com/firebasejs/ui/2.0.0/images/auth/google.svg" alt="Google Icon" class="h-5 w-5 mr-2">
+                <span class=" font-medium text-gray-600">Continue With Google</span>
+              </a>
+
+
+            <div class="h-6"></div>
+              <div class="text-center">
+                <span class="text-sm text-gray-500">Already have an account?</span>
+                <a href="{{route('login')}}" class="text-sm text-theme-green font-bold hover:underline text-kaitoke-green-700">Login</a>
+              </div>
+
+
+
+
+                </form>
+
+
+            </div>
+
 
             <!-- Right Side Image -->
             <div class="w-1/2 hidden md:block">
@@ -70,4 +75,4 @@
             </div>
         </div>
     </div>
-</x-guest-layout> 
+</x-guest-layout>

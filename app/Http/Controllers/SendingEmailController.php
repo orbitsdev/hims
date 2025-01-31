0d8d34560f8511f62efd3c4bb243882f6c5d980e
Brian Orbino <86046953+orbitsdev@users.noreply.github.com>
2025-01-31 15:19:19 +0800

    wip

--- app/Http/Controllers/SendingEmailController.php
+++ app/Http/Controllers/SendingEmailController.php
@@ -10,24 +10,24 @@ class SendingEmailController extends Controller
 {
     public static function sendBPAlertEmail($record){
 
-        if($record->getBloodPressureStatus() != 'Normal'){
-
-            try {
-
-                $suggestion = $record->getBloodPressureSuggestion();
-                $user = $record->user;
+        // if($record->getBloodPressureStatus() != 'Normal'){
 
 
-                  Mail::to($user->email)->send(new BloodPressureStatus($user->fullName(), $record->getBloodPressureStatus(), $suggestion));
+
+        // }
+        try {
+
+            $suggestion = $record->getBloodPressureSuggestion();
+            $user = $record->user;
 
 
-                FilamentForm::notification('Email was sent successfully.');
-            } catch (\Exception $e) {
-                FilamentForm::error('Failed to send email. Please try again later.');
-            }
+              Mail::to($user->email)->send(new BloodPressureStatus($user->fullName(), $record->getBloodPressureStatus(), $suggestion));
 
+
+            FilamentForm::notification('Email was sent successfully.');
+        } catch (\Exception $e) {
+            FilamentForm::error('Failed to send email. Please try again later.');
         }
-
 
     }
 }
